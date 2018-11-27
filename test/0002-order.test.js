6c1524b8c9439a0f7f5868854d242ffca2a5127b
Austin Wright <aaa@bzfx.net>
2018-11-26 23:26:47 -0500

    Add tests for sorting & empty result set
    The empty result set one errors right now. Will fix later.

---
+++ test/0002-order.test.js
@@ -0,0 +1,45 @@
+
+
+var assert = require('assert');
+var fs = require('fs');
+
+var rdf = require('rdf');
+var parse = require('../index.js').parse;
+var XMLSerializer = require('xmldom').XMLSerializer;
+var DOMParser = require('xmldom').DOMParser;
+
+var baseIRI = 'http://example.com/';
+
+// TODO: load this data in a before() clause
+var templateFilename = __dirname + '/data-table.html';
+var templateContents = fs.readFileSync(templateFilename, 'UTF-8');
+var sz = new XMLSerializer;
+var document = new DOMParser().parseFromString(templateContents, 'text/xml');
+
+var dataFilename = __dirname + '/data-table.ttl';
+var dataContents = fs.readFileSync(dataFilename, 'UTF-8');
+var dataParse = rdf.TurtleParser.parse(dataContents, baseIRI);
+var dataGraph = dataParse.graph;
+
+var rdfenv = {
+	createNamedNode: rdf.environment.createNamedNode,
+	createBlankNode: rdf.environment.createBlankNode,
+	createLiteral: rdf.environment.createLiteral,
+}
+
+describe("0002", function(){
+    it("genreateDocument ASC(?name)", function(){
+        var result = parse(baseIRI, document);
+        var rdoc = result.parser.generateDocument(result.document, dataGraph);
+        var eBody = rdoc.documentElement.childNodes[3];
+        assert.equal(eBody.nodeName, 'body');
+        var eMain = rdoc.getElementById('main-content');
+        assert.equal(eMain.nodeName, 'main');
+        var eTr = rdoc.getElementsByTagName('tr');
+        assert.equal(eTr.length, 4);
+        assert.equal(eTr[0].childNodes[0].firstChild.textContent, 'Name');
+        assert.equal(eTr[1].childNodes[1].firstChild.textContent, 'Alice');
+        assert.equal(eTr[2].childNodes[1].firstChild.textContent, 'Bob');
+        assert.equal(eTr[3].childNodes[1].firstChild.textContent, 'Charlie');
+    });
+});
