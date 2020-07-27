fecf827b9ef266f8f9e814feaa2b10fe270da1c0
Austin Wright <aaa@bzfx.net>
2020-07-26 19:53:30 -0700

    Add eslintrc and appease

---
+++ test/.eslintrc.js
@@ -0,0 +1,22 @@
+"use strict";
+module.exports = {
+	"env": {
+		"mocha": true,
+		"es6": true,
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 8,
+	},
+	"rules": {
+		"indent": [ "error", "tab", { SwitchCase: 1 } ],
+		"strict": ["error", "global"],
+		"no-unused-vars": [ "warn" ],
+		"no-unreachable": [ "warn" ],
+		"linebreak-style": [  "error", "unix" ],
+		"semi": [ "error", "always" ],
+		"no-extra-semi": [ "error" ],
+		"comma-dangle": [ "error", "always-multiline" ],
+		"no-console": [ "warn" ],
+	},
+};
